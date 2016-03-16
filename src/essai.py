85646b09c74092dd8cfa44d81d0c607daaec62ec
Thomas Martin <mt.thomas.martin@gmail.com>
2016-03-16 03:17:25 +0100

    according to essai.py all base functions, except weighting, work

--- src/essai.py
+++ src/essai.py
@@ -24,17 +24,6 @@ def cluster_points(X, mu):
             clusters[bestmukey] = [x]
     return clusters
 
-    # def cluster_points(X, mu):
-    # clusters  = {}
-    # for x in X:
-    #     bestmukey = min([(i[0], np.linalg.norm(x-mu[i[0]])) \
-    #                 for i in enumerate(mu)], key=lambda t:t[1])[0]
-    #     try:
-    #         clusters[bestmukey].append(x)
-    #     except KeyError:
-    #         clusters[bestmukey] = [x]
-    # return clusters
-
 
 def reevaluate_centers(mu, clusters):
     newmu = []
@@ -59,18 +48,37 @@ def vec_mean(cluster, dim = 64):
             sum.append(0)
     return(sum)
 
+def has_converged(mu, oldmu):
+    return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu]))
+
+def find_centers(X, K):
+    # Initialize to K random centers
+    dim = 64
+    oldmu = random_centers(K, dim)
+    mu = random_centers(K, dim)
+    while not has_converged(mu, oldmu):
+        oldmu = mu
+        # Assign all points in X to clusters
+        clusters = cluster_points(X, mu)
+        # Reevaluate centers
+        mu = reevaluate_centers(oldmu, clusters)
+    return(mu, clusters)
+
 trainingset=[]
-direction = "/Users/Thomartin/mopsi/images/tour_eiffel/tour_eiffel_3.jpg"
+direction = "/Users/Thomartin/mopsi/images/tour_eiffel"
 surf = cv2.xfeatures2d.SURF_create()
-img = cv2.imread(direction)
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-(kps,descs) = surf.detectAndCompute(gray, None)
-for j in range(0,len(descs)):
-    trainingset.append((kps[j],descs[j],direction))
+trainingset = []
+for file in os.listdir(direction):
+    img = cv2.imread(direction+"/"+file)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    (kps,descs) = surf.detectAndCompute(gray, None)
+    for j in range(0,len(descs)):
+        trainingset.append((kps[j],descs[j],direction+"/"+file))
 
 print(type(trainingset[4][1].tolist()[1]))
 cen = random_centers(4, 64)
 clus = cluster_points(trainingset, cen)
-print(reevaluate_centers(cen, clus ))
-print(len(cen[0]))
+# print(reevaluate_centers(cen, clus ))
+print(cen)
 # print(clus.keys())
+print(find_centers(trainingset, 3)[0])
