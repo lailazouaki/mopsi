d0b85a1d384f630fc4aa1088b9169d5810b87bfa
Laila Zouaki <laila.zouaki@gmail.com>
2016-03-17 18:21:31 +0100

    Nettoyage et réarrangement du code. Possibilité d'enregistrer les arbres entrainés. Tests avec différents L, K et taille d'image database

---
+++ src/script_overnight.sh
@@ -0,0 +1,7 @@
+python KMean_training.py 2 2 small 1
+python KMean_training.py 2 3 small 2
+python KMean_training.py 2 10 small 3
+python KMean_training.py 6 10 small 4
+python KMean_training.py 6 10 large 5
+python KMean_training.py 2 2 large 6
+python KMean_training.py 2 3 large 7
--- src/searchTree.py
+++ src/searchTree.py
@@ -1,9 +1,12 @@
-import cv2
-import numpy as np
 from os import listdir
 from os.path import isfile, join
+
+import cv2
 import sys
 import featureMatching
+import operator 
+
+import numpy as np
 import cPickle as pickle
 
 
@@ -14,8 +17,24 @@ def openObject(filename):
 		openedObject = pickle.load(obj)
 
 	return openedObject
+<<<<<<< HEAD
 
 
+def find_nearest_center(list_subTree, descriptor):
+    return min([(i, np.linalg.norm(descriptor-list_subTree[i][0])) \
+                    for i, subtree in enumerate(list_subTree)], key=lambda t:t[1])[0]
+
+=======
+>>>>>>> Nettoyage et réarrangement du code. Possibilité d'enregistrer les arbres entrainés. Tests avec différents L, K et taille d'image database
+
+def getDicScore(trained_tree, descriptor, max_depth=2):
+    tree = trained_tree
+    for i in range(0, max_depth):
+        branchNumber = find_nearest_center(tree, descriptor)
+        tree = tree[branchNumber][1]
+
+<<<<<<< HEAD
+=======
 def find_nearest_center(list_subTree, descriptor):
     return min([(i, np.linalg.norm(descriptor-list_subTree[i][0])) \
                     for i, subtree in enumerate(list_subTree)], key=lambda t:t[1])[0]
@@ -27,6 +46,7 @@ def getDicScore(trained_tree, descriptor, max_depth=2):
         branchNumber = find_nearest_center(tree, descriptor)
         tree = tree[branchNumber][1]
 
+>>>>>>> Nettoyage et réarrangement du code. Possibilité d'enregistrer les arbres entrainés. Tests avec différents L, K et taille d'image database
     return tree 
 
 # Score de toutes les images pour un descripteur
@@ -44,6 +64,7 @@ def incrementTotal(relevance_database, trained_tree, decs):
 if __name__ == "__main__":
 		
 	trained_tree = openObject('trained_tree.pkl')
+<<<<<<< HEAD
 
 	surf = cv2.xfeatures2d.SURF_create()
 	gray = cv2.imread("/Users/lailazouaki/Documents/MOPSI/images/ndp_7.jpg", 0)
@@ -60,6 +81,28 @@ if __name__ == "__main__":
 	print(relevance_database)
 	print(max(relevance_database.values()))
 	print(relevance_database["/Users/lailazouaki/Documents/MOPSI/images/image_all/ndp_7.jpg"])
+=======
+	print(trained_tree)
+	surf = cv2.xfeatures2d.SURF_create()
+	gray = cv2.imread("/Users/lailazouaki/Documents/MOPSI/images/ndp_1.jpg", 0)
+	(kps,descs) = surf.detectAndCompute(gray, None)
+
+	# Initialisation du dictionnaire de pertinence des images de la base de donnees
+	relevance_database={}
+	for img in listdir("/Users/lailazouaki/Documents/MOPSI/images/image_all"):
+	    relevance_database["/Users/lailazouaki/Documents/MOPSI/images/image_all/"+img] = 0
+
+
+	# Calcul du score des images de la base de donnees pour la query image
+	incrementTotal(relevance_database, trained_tree, descs)
+	# sorted_relevance = sorted(relevance_database.items(), key=operator.itemgetter(1))
+
+	print(relevance_database.values())
+	print("-----------------------")
+	print(max(relevance_database.values()))
+	# for element in reversed(sorted_relevance):
+	# 	print(element)
+>>>>>>> Nettoyage et réarrangement du code. Possibilité d'enregistrer les arbres entrainés. Tests avec différents L, K et taille d'image database
 
 
 
